a5571f74d6227cde61c2e1e162adff75fac7a6f6
kuchinashi-gadget <kuchinashi.gadget@gmail.com>
2016-09-29 15:06:41 +0900

    Initial Commit

---
+++ apple_store/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apple_store
+//
+//  Created by KosukeOkano on 2016/09/29.
+//  Copyright © 2016年 岡野光祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
